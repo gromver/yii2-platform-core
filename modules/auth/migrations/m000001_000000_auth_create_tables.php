c408b47bba987afe386d620d96c6d9b74f1e19b6
Roman <roman.tlt-84@mail.ru>
2016-03-02 12:31:15 +0400

    fix auth migration

--- modules/auth/migrations/m000001_000000_auth_create_tables.php
+++ modules/auth/migrations/m000001_000000_auth_create_tables.php
@@ -28,7 +28,24 @@ class m000001_000000_auth_create_tables extends \yii\db\Migration
         ]);
 
         $this->addForeignKey('Core_AuthItem_RuleName_fk', '{{%core_auth_item}}', 'rule_name', '{{%core_auth_rule}}', 'name', 'SET NULL', 'CASCADE');
-        $this->createIndex('AuthItem_idx', '{{%core_auth_item}}', 'type');
+        $this->createIndex('Type_idx', '{{%core_auth_item}}', 'type');
+
+        // auth item child
+        $this->createTable('{{%core_auth_item_child}}', [
+            'parent' => Schema::TYPE_STRING . '(64) NOT NULL',
+            'child' => Schema::TYPE_STRING . '(64) NOT NULL',
+            'PRIMARY KEY (parent,child)',
+        ]);
+        $this->addForeignKey('Core_AuthItemChild_Parent_fk', '{{%core_auth_item_child}}', 'parent', '{{%core_auth_item}}', 'name', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('Core_AuthItemChild_Child_fk', '{{%core_auth_item_child}}', 'child', '{{%core_auth_item}}', 'name', 'CASCADE', 'CASCADE');
+        // auth assignment
+        $this->createTable('{{%core_auth_assignment}}', [
+            'item_name' => Schema::TYPE_STRING . '(64) NOT NULL',
+            'user_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'created_at' => Schema::TYPE_INTEGER,
+            'PRIMARY KEY (item_name,user_id)',
+        ]);
+        $this->addForeignKey('Core_AuthAssignment_ItemName_fk', '{{%core_auth_assignment}}', 'item_name', '{{%core_auth_item}}', 'name', 'CASCADE', 'CASCADE');
     }
 
     public function down()
